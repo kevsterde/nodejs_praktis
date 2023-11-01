13a064a7c5033300a19ee1e5b51c78895c1af106
KevinRevalde <kewrevalde@mail.com>
2023-11-01 16:54:24 +0800

    overview

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -7,8 +7,30 @@ const fs = require('fs');
 
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
-
 const dataObj = JSON.parse(data);
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProd = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+const replaceTemplate = (temp,product) => {
+    let output = temp.replace(/{%PROPS.NAME%}/g,product.productName);
+    output = output.replace(/{%PROPS.PLACE%}/g,product.from);
+    output = output.replace(/{%PROPS.IMG%}/g,product.image);
+    output = output.replace(/{%PROPS.NUTRI%}/g,product.nutrients);
+    output = output.replace(/{%PROPS.QTY%}/g,product.quantity);
+    output = output.replace(/{%PROPS.PRICE%}/g,product.price);
+    output = output.replace(/{%PROPS.DESC%}/g,product.description);
+    output = output.replace(/{%PROPS.ID%}/g,product.id);
+
+    if(!product.organic) output = output.replace(/{%PROPS.ORGANIC%}/g,'not-organic');
+
+    return output;
+
+
+}
+
 
 
 const server = http.createServer((req,res)=>{
@@ -19,15 +41,37 @@ const server = http.createServer((req,res)=>{
     if(pathname === '/overview' || pathname === '/')
     {
 
-        res.end('This is the OVERVIEW');
+        res.writeHead(200,{
+            'Content-type':'text/html',
+        });
+
+
+        const cardsHTML = dataObj.map(item=> replaceTemplate(tempCard,item)).join('');
+        const finalOverView = tempOverview.replace(/{%PRODUCT_CARDS%}/g,cardsHTML);
+
+        res.end(finalOverView);
+
+
+
+
     }
     else if(pathname === '/product')
     {
 
-        res.end('This is the PRODUCTI');
+
+        res.writeHead(200,{
+            'Content-type':'text/html',
+        });
+
+        res.end(tempProd);
+
+
+
     }
     else if (pathname === '/api')
     {
+
+
 
             res.writeHead(200,{
                 'Content-type': 'application/json',
@@ -37,11 +81,18 @@ const server = http.createServer((req,res)=>{
 
     }
     else{
+
+
+
+
         res.writeHead(404,{
             'Content-type': 'text/html',
             'my-own-header': 'hello-world',
         });
         res.end('<h1>Page Not Found!</h1>');
+
+
+
     }
 
 
